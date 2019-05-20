13b908d16e562ed93552e18b330782e2b9423938
George Ruiz <georuiz817@gmail.com>
2019-05-20 14:49:22 +0000

    Automatically backed up by Learn

--- for.rb
+++ for.rb
@@ -1,10 +1,17 @@
-
-def using_for
-	checklist = 1..10
-	for item in checklist
-	 puts "Wingardium Leviosa"
-	
+def loop_iterator(number_of_times)
+  number_of_times = 0
+  loop do 
+    number_of_times += 1
+    puts "Welcome to Flatiron School's Web Development Course!"
+    if number_of_times >= 10
+      break
+    end
+  end
 end
-end
-
-
+  def times_iterator(number_of_times)
+    10.times do 
+      phrase= "Welcome to Flatiron School's Web Development Course!"
+      puts phrase 
+    end
+  end
+ 
